fec8fda7b97621d364aed1d055078e7a7b5a8381
fufufukakaka <demonstrundum@gmail.com>
2021-03-18 20:38:24 +0900

    add ingredient-container

---
+++ components/organisms/ingredient-container.tsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled'
+import { Ingredients } from '~/components/molecules/ingredients'
+import { Header } from '~/components/molecules/header'
+import { Divider } from '../atoms/divider'
+
+export type IngredientContainerPropType = {
+  ingredientsInfo: {
+    ingredientName: string
+    ingredientUnit: string
+  }[]
+}
+
+export const IngredientContainer: React.VFC<IngredientContainerPropType> = ({
+  ingredientsInfo,
+}) => {
+  return (
+    <Container>
+      <Header headerText="材料" colorType="light" textType="14bold" />
+      {ingredientsInfo.map((info, index) => (
+        <>
+          <Ingredients
+            key={index}
+            ingredientName={info.ingredientName}
+            ingredientUnit={info.ingredientUnit}
+          />
+          <Divider />
+        </>
+      ))}
+    </Container>
+  )
+}
+
+const Container = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  flexWrap: 'wrap',
+})
