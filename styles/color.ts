672befffd7959f03946490dc4998c63734f7778e
fufufukakaka <demonstrundum@gmail.com>
2021-03-18 15:12:52 +0900

    add icon components

--- styles/color.ts
+++ styles/color.ts
@@ -22,8 +22,16 @@ export const colors = {
     primary: '#FAFAF5',
     accent: '#F78700',
   },
+  icon: {
+    primary: '#1C1A10',
+    white: '#ffffff',
+    accent: '#F78700',
+    gray: '#989483',
+    green: '#97AF10',
+  },
 }
 
 export type BaseColor = keyof typeof colors.base
 export type TextColor = keyof typeof colors.text
 export type BackgroundColor = keyof typeof colors.background
+export type IconColor = keyof typeof colors.icon
