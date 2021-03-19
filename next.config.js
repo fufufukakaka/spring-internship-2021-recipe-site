75d1c56c6bb1c18760a992848ff24aaf0b663106
fufufukakaka <demonstrundum@gmail.com>
2021-03-19 13:55:12 +0900

    resolve cors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 module.exports = {
-  async rewrites() {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  async headers() {
     return [
       {
-        source: '/:path*',
-        destination: 'https://internship-recipe-api.ckpd.co/:path*',
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
       },
     ]
   },
